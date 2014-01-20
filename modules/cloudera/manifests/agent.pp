575a4bab6ee054d9e4ed7fe97074b33c4328adc4
Rob Sletten <rsletten@gmail.com>
2014-01-20 15:46:50 -0500

    added cloudera::class

--- modules/cloudera/manifests/agent.pp
+++ modules/cloudera/manifests/agent.pp
@@ -1,27 +1,30 @@
-class cloudera::agent::service {
-  service { "cloudera-scm-agent":
-    ensure     => running,
-    hasstatus  => true,
-    hasrestart => true,
-    enable     => true,
-    require    => Class["cloudera::agent::package"],
-  }
-}
+class cloudera::agent {
 
-class cloudera::agent::config {
-  file { "/etc/cloudera-scm-agent/config.ini":
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => 0644,
-    source  => "puppet:///modules/cloudera/config.ini",
-    require => Class["cloudera::agent::package"],
-    notify  => Class["cloudera::agent::service"],
+  class cloudera::agent::service {
+    service { "cloudera-scm-agent":
+      ensure     => running,
+      hasstatus  => true,
+      hasrestart => true,
+      enable     => true,
+      require    => Class["cloudera::agent::package"],
+    }
   }
-}
 
-class cloudera::agent::package {
-  package { "cloudera-manager-agent":
-    ensure => present,
+  class cloudera::agent::config {
+    file { "/etc/cloudera-scm-agent/config.ini":
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => 0644,
+      source  => "puppet:///modules/cloudera/config.ini",
+      require => Class["cloudera::agent::package"],
+      notify  => Class["cloudera::agent::service"],
+    }
+  }
+
+  class cloudera::agent::package {
+    package { "cloudera-manager-agent":
+      ensure => present,
+    }
   }
 }
