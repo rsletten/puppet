ba3c5471ce6a705de35d421d0cc8578f5b3d819c
Rob Sletten <rsletten@camstar.com>
2014-01-20 16:29:22 -0500

    added cloudera::agent include to all WNs

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,15 +29,19 @@ node 'csc-mn02.saas.local' inherits hadoop {
 
 #
 node 'csc-wn00.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn01.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn02.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn03.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn04.saas.local' inherits hadoop {
@@ -49,102 +53,135 @@ node 'csc-wn05.saas.local' inherits hadoop {
 }
 
 node 'csc-wn06.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn07.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn08.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn09.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn10.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn11.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn12.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn13.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn14.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn15.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn16.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn17.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn18.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn19.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn20.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn21.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn22.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn23.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn24.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn25.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn26.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn27.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn28.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn29.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn30.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn31.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn32.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn33.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn34.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn35.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn36.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn37.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-wn38.saas.local' inherits hadoop {
+  include cloudera::agent
 }
 
 node 'csc-sn03.saas.local' {
