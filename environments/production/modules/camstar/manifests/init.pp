622c098ab44e553db13ee1fc5b5fd1b60027dcdb
Rob Sletten <rsletten@camstar.com>
2014-02-11 15:50:05 -0500

    added rsletten sshkey

--- environments/production/modules/camstar/manifests/init.pp
+++ environments/production/modules/camstar/manifests/init.pp
@@ -193,5 +193,27 @@ class camstar::accounts {
   realize(User['sseedharalu'])
   realize(User['kdevalk'])
   realize(User['naghazarian'])
+
+  ssh_authorized_key { 'rsletten':
+    user   => "rsletten",
+    ensure => present,
+    type   => "ssh-rsa",
+    key    => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDLoNDo+HkY+nEQ5jd89NtJxdp6B/fjNMoKuxV+1ovtdrjmGp7CYYe39GwvMqTFi3vxjL+uWJYbIEkPdidbN/IdmSeNyGIILCVSe8PUAgJF2nWofiWsBvflHHWdow3vhIF/k+WBRO39vaq86Qln7I19qtI2QnPzgLgNW0uuUEyIjeKpIvDy33gajGWQoOyTh4ACibsgu8yf+YTgykIq+a0xDf/O6rngQsdbiXZdbm8j8sx81p9q3vVo/2wk2qywiYdx24X5aEedSnQqT6U/cbhLHHq0LM+ZvMvDCfOY4dsraRucj8rg1GxSRn9XezEuv3njd0HK8uoREWP8ch6M2+gN",
+    name   => "rsletten"
+  }
+
 }
 
+class camstar::rsletten {
+  require camstar::users
+  realize(Group['rsletten'])
+  realize(User['rsletten'])
+  ssh_authorized_key { 'rsletten':
+    user   => "rsletten",
+    ensure => present,
+    type   => "ssh-rsa",
+    key    => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDLoNDo+HkY+nEQ5jd89NtJxdp6B/fjNMoKuxV+1ovtdrjmGp7CYYe39GwvMqTFi3vxjL+uWJYbIEkPdidbN/IdmSeNyGIILCVSe8PUAgJF2nWofiWsBvflHHWdow3vhIF/k+WBRO39vaq86Qln7I19qtI2QnPzgLgNW0uuUEyIjeKpIvDy33gajGWQoOyTh4ACibsgu8yf+YTgykIq+a0xDf/O6rngQsdbiXZdbm8j8sx81p9q3vVo/2wk2qywiYdx24X5aEedSnQqT6U/cbhLHHq0LM+ZvMvDCfOY4dsraRucj8rg1GxSRn9XezEuv3njd0HK8uoREWP8ch6M2+gN",
+    name   => "rsletten"
+  }
+
+}
