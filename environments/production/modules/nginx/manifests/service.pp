0ca42c3d2cd421d677d8592c41b5e530f1469e52
Rob Sletten <rsletten@gmail.com>
2014-02-13 11:36:15 -0500

    corrected nginx::service

--- environments/production/modules/nginx/manifests/service.pp
+++ environments/production/modules/nginx/manifests/service.pp
@@ -2,7 +2,7 @@ class nginx::service {
   service { 'nginx':
     ensure    => 'running',
     hasstatus => 'true',
-    harestart => 'true',
+    hasrestart => 'true',
     enable    => 'true',
     require   => Class['nginx::package'],
   }
