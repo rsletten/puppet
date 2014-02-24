5a356f2afe15fbd44f1cf032004ef3ebfce0773f
Rob Sletten <rsletten@gmail.com>
2014-02-24 09:12:55 -0500

    added /nginx to jetty-0

--- manifests/site.pp
+++ manifests/site.pp
@@ -173,7 +173,14 @@ node 'csc-sn03.saas.local' {
 }
 
 node 'csc-jetty-0.saas.local' {
+  $falconwebservices = {
+    node1 => { 'server' => '10.40.20.15', port => '8080' },
+    node2 => { 'server' => '10.40.20.16', port => '8080' },
+    node3 => { 'server' => '10.40.20.17', port => '8080' },
+  }
+  $balanceport = '8085'
   include role::virtual
+  include nginx
 }
 
 node 'csc-jetty-1.saas.local' {
