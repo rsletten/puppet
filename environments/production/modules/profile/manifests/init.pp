57d012c50ab7db2d8be232a31b72fd4bd0fe319d
Rob Sletten <rsletten@gmail.com>
2014-02-27 08:40:58 -0500

    added profile::gituser

--- environments/production/modules/profile/manifests/init.pp
+++ environments/production/modules/profile/manifests/init.pp
@@ -160,6 +160,26 @@ class profile::rootpass {
   }
 }
 
+class profile::gituser {
+  @group { 'git':
+    ensure => present,
+  }
+
+  @user { 'git':
+    ensure => present,
+  }
+
+  realize(Group['git'])
+  realize(User['git'])
+
+  ssh_authorized_key { 'git':
+    user   => "git",
+    ensure => present,
+    type   => "ssh-rsa"
+    key    => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDLoNDo+HkY+nEQ5jd89NtJxdp6B/fjNMoKuxV+1ovtdrjmGp7CYYe39GwvMqTFi3vxjL+uWJYbIEkPdidbN/IdmSeNyGIILCVSe8PUAgJF2nWofiWsBvflHHWdow3vhIF/k+WBRO39vaq86Qln7I19qtI2QnPzgLgNW0uuUEyIjeKpIvDy33gajGWQoOyTh4ACibsgu8yf+YTgykIq+a0xDf/O6rngQsdbiXZdbm8j8sx81p9q3vVo/2wk2qywiYdx24X5aEedSnQqT6U/cbhLHHq0LM+ZvMvDCfOY4dsraRucj8rg1GxSRn9XezEuv3njd0HK8uoREWP8ch6M2+gN",
+    name   => "git"
+  }
+
 class profile::rootsshkey {
   ssh_authorized_key { 'root':
     user   => "root",
