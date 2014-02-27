b4b6d7bba8bddda6406bc2fff397eae7325057d9
Rob Sletten <rsletten@gmail.com>
2014-02-27 08:46:45 -0500

    corrected gituser

--- environments/production/modules/profile/manifests/init.pp
+++ environments/production/modules/profile/manifests/init.pp
@@ -171,9 +171,9 @@ class profile::gituser {
   ssh_authorized_key { 'git':
     user   => "git",
     ensure => present,
-    type   => "ssh-rsa"
+    type   => "ssh-rsa",
     key    => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDLoNDo+HkY+nEQ5jd89NtJxdp6B/fjNMoKuxV+1ovtdrjmGp7CYYe39GwvMqTFi3vxjL+uWJYbIEkPdidbN/IdmSeNyGIILCVSe8PUAgJF2nWofiWsBvflHHWdow3vhIF/k+WBRO39vaq86Qln7I19qtI2QnPzgLgNW0uuUEyIjeKpIvDy33gajGWQoOyTh4ACibsgu8yf+YTgykIq+a0xDf/O6rngQsdbiXZdbm8j8sx81p9q3vVo/2wk2qywiYdx24X5aEedSnQqT6U/cbhLHHq0LM+ZvMvDCfOY4dsraRucj8rg1GxSRn9XezEuv3njd0HK8uoREWP8ch6M2+gN",
-    name   => "git"
+    name   => "git",
   }
 }
 
@@ -183,7 +183,7 @@ class profile::rootsshkey {
     ensure => present,
     type   => "ssh-rsa",
     key    => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDLoNDo+HkY+nEQ5jd89NtJxdp6B/fjNMoKuxV+1ovtdrjmGp7CYYe39GwvMqTFi3vxjL+uWJYbIEkPdidbN/IdmSeNyGIILCVSe8PUAgJF2nWofiWsBvflHHWdow3vhIF/k+WBRO39vaq86Qln7I19qtI2QnPzgLgNW0uuUEyIjeKpIvDy33gajGWQoOyTh4ACibsgu8yf+YTgykIq+a0xDf/O6rngQsdbiXZdbm8j8sx81p9q3vVo/2wk2qywiYdx24X5aEedSnQqT6U/cbhLHHq0LM+ZvMvDCfOY4dsraRucj8rg1GxSRn9XezEuv3njd0HK8uoREWP8ch6M2+gN",
-    name   => "root"
+    name   => "root",
   }
 
   file { '/root/.ssh/id_rsa':
